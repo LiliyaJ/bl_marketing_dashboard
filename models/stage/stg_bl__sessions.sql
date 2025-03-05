b95060393561d8ef51adc245f24a5ebc36b09bc6
Liliya Jeromin <zl108108@gmail.com>
2025-03-05 10:52:18 +0100

    staging

--- models/stage/stg_bl__sessions.sql
+++ models/stage/stg_bl__sessions.sql
@@ -1,6 +1,11 @@
 with 
     source as (
 
+        select * from {{ source('bl_marketing_dashboard', 'sessions') }}
+
+    ),
+
+    transformed as (
         select
         
             session_date,
@@ -8,12 +13,25 @@ with
             channel,
             device,
             leasing_contract_id,
-            sum(costs) costs,
-            count(distinct session_id) num_of_sessions,
-            countif(leasing_contract_id is not null) contracts
+
+            -- user visits sequence
+            row_number() over(
+            partition by user_id
+            order by session_date, user_id
+            ) user_visits_seq
+
+            -- count costs per user, oper date, per channel, per device, per contract
+           -- sum(costs) costs,
+
+            -- count number of sessions per user, oper date, per channel, per device, per contract
+           -- count(distinct session_id) num_of_sessions,
+
+            -- count contracts
+           -- countif(leasing_contract_id is not null) contracts
         
-        from {{ source('bl_marketing_dashboard', 'sessions') }}
-        group by  session_date, user_id, channel, device, leasing_contract_id
+        from source
+       -- group by session_date, user_id, 
+       --         channel, device, leasing_contract_id
     )
 
-select * from source
+select * from transformed
